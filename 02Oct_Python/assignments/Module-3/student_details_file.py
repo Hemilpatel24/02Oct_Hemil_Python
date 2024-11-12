0dba5df005aa047f94331c1cdfb8495cafc6e447
Hemilpatel24 <hemilpatel61024@gmail.com>
2024-11-12 21:31:55 +0530

    Module-2

--- 02Oct_Python/assignments/Module-3/student_details_file.py
+++ 02Oct_Python/assignments/Module-3/student_details_file.py
@@ -1,10 +1,12 @@
 """fl = open("studentsdata.txt", 'x')"""
 
 fl = open("studentdata.txt", 'a')
+n = int(input("Enter number of students:"))
 
-ID = input("Enter your id:")
-NAME = input("Enter your name:")
-CITY = input("Enter your city:")
+for i in range(n):
+    ID = input("Enter your id:")
+    NAME = input("Enter your name:")
+    CITY = input("Enter your city:")
 
 fl.write(f"ID:{ID}\nNAME:{NAME}\nCITY:{CITY}\n=================================\n")
 
