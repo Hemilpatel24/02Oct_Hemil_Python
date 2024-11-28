210b1cce9871965caf733b06df2971fee988d3c9
Hemilpatel24 <hemilpatel61024@gmail.com>
2024-11-28 15:55:36 +0530

    Module-4

---
+++ 02Oct_Python/assignments/Module-3_OOP/my_tinker.py
@@ -0,0 +1,9 @@
+import tkinter
+
+screen=tkinter.Tk()
+screen.geometry("400x500")
+screen.title("My Application")
+screen.config(background="white")
+tkinter.Label(text="FirstName").pack()
+screen.mainloop()
+
