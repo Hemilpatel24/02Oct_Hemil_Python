efa18cb4149c0c762e2db8f4dced7894ef135d20
Hemilpatel24 <hemilpatel61024@gmail.com>
2024-12-10 16:29:22 +0530

    Module-4

--- 02Oct_Python/assignments/Module-1/task5.py
+++ 02Oct_Python/assignments/Module-1/task5.py
@@ -1,15 +1,6 @@
-"""n=1
+n=1
 for i in range(1,6):
     for j in range(i):
         print(n, end=" ")
         n+=1
-    print(" ")"""
-#--------------------------#
-def pattern(n):
-    for i in range(n+1):
-        for j in range(i):
-            print('*', end=" ")
-        print(" ")
-
-n = int(input("Enter any number:"))
-print(pattern(n))
+    print(" ")
